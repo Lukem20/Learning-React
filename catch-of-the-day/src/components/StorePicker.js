958280d3e795a80a13543f86053e6259c50a3461
Luke <lukemoore237@gmail.com>
2022-05-25 13:21:19 -0700

    First component!

---
+++ catch-of-the-day/src/components/StorePicker.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class StorePicker extends React.Component {
+    render () {
+        return <p>Hello! I'm StorePicker component.</p>
+    }
+}
+
+export default StorePicker;
