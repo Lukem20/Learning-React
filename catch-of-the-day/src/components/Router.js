a911066df92835203e7834935e0b34c8f72f55df
Luke <lukemoore237@gmail.com>
2022-06-29 12:49:19 -0700

    adding correct path?

--- catch-of-the-day/src/components/Router.js
+++ catch-of-the-day/src/components/Router.js
@@ -7,7 +7,11 @@ import NotFound from "./NotFound";
 const Router = () => (
   <BrowserRouter>
     <Switch>
-      <Route exact path="/" component={StorePicker} />
+      <Route
+        exact
+        path="https://learning-react-beta.vercel.app/"
+        component={StorePicker}
+      />
       <Route path="/store/:storeId" component={App} />
       <Route component={NotFound} />
     </Switch>
